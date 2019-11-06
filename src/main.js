e6000df40c16a19ce4ddd2cbb3112f2ae477d4e7
sidorovi4 <vladsidorovi4@gmail.com>
2019-11-06 22:47:39 +0300

    added firebase authentification

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database'
 import App from './App.vue';
 import router from './router/index';
-import store from './store/index';
+import store from './store';
 import dateFilter from './filtes/date.filter';
 import messagePlugin from './utils/message.plugin'
 import './registerServiceWorker';
@@ -14,8 +17,25 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: 'AIzaSyA8f5o8Z842EZSFjaIyDJ5X-r-34j0g628',
+  authDomain: 'virtualpurse-e0e4e.firebaseapp.com',
+  databaseURL: 'https://virtualpurse-e0e4e.firebaseio.com',
+  projectId: 'virtualpurse-e0e4e',
+  storageBucket: 'virtualpurse-e0e4e.appspot.com',
+  messagingSenderId: '238178934107',
+  appId: '1:238178934107:web:852fe504143046ce66ce7e',
+  measurementId: 'G-0DCR45GLGB'
+})
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App),
+    }).$mount('#app');
+  }
+})
